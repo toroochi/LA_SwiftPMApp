6305aee15583f599cbc26e227444fd045d300ce8
yamaokana <kudanobo11@outlook.jp>
2022-09-02 02:02:17 +0900

    Initial Commit

---
+++ SwiftPMApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftPMApp
+//
+//  Created by 山尾かな on 2022/09/02.
+//
+
+import UIKit
+import PKHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        HUD.flash(.success, delay: 2.0)
+    }
+}
+
